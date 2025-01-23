0ed9209e6b7095e06792382fb8d7e4f0b772a271
bellfollow <jongjun4959@gmail.com>
2025-01-23 17:08:16 +0900

    final_quiz

--- Quiz_Game/question_model.py
+++ Quiz_Game/question_model.py
@@ -1,5 +1,9 @@
 class Question:
-
     def __init__(self, q_text, q_answer):
         self.text = q_text
         self.answer = q_answer
+
+    def __repr__(self):
+        return self.text
+    #자꾸 객체 주소가 반환되서 찾아보니 __repr__가 빠져있었음 
+    #Question 클래스에 __str__ 또는 __repr__ 메서드가 정의되어 있지 않으므로, print나 f-string에서 해당 인스턴스를 출력할 때 객체의 메모리 주소가 반환됩니다.
--- Quiz_Game/quiz_brain.py
+++ Quiz_Game/quiz_brain.py
@@ -1,25 +1,28 @@
 class QuizBrain:
-
-    def __init__(self, q_list):
+    def __init__(self,q_list):
         self.question_number = 0
         self.score = 0
         self.question_list = q_list
-
+    
     def still_has_questions(self):
-        return self.question_number < len(self.question_list)
-
+        # if self.question_number < len(self.question_list):
+        #     return True
+        # else:
+        #     False 
+        # 위 코드를 더 간결하게 한다면
+        return self.question_number < len(self.question_list) #충족 못하는 순간 False출력 
+    
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
-        self.check_answer(user_answer, current_question.answer)
-
-    def check_answer(self, user_answer, correct_answer):
+        user_answer = input(f"Q.{self.question_number}:  {current_question} (True/False)" )
+        self.check_answer(user_answer,current_question.answer)
+    
+    def check_answer(self,user_answer,correct_answer):
         if user_answer.lower() == correct_answer.lower():
             self.score += 1
-            print("You got it right!")
+            print("You got it right")
         else:
-            print("That's wrong.")
-        print(f"The correct answer was: {correct_answer}.")
-        print(f"Your current score is: {self.score}/{self.question_number}")
-        print("\n")
+            print("That's worng")
+        print(f"김춘수(맞춘 수): {correct_answer}")
+        print(f"스코어: {self.score}/ {self.question_number}")
